c1b81c9148dca2f247bbb1898ac03ad681e416bc
AShaver43 <gofrodude_197@hotmail.com>
2020-07-14 12:21:44 -0700

    Characters (Enemy) done

--- js/main.js
+++ js/main.js
@@ -17,16 +17,33 @@ class NPC {
         this.life = true
         this.speed = 0
     }
+    changeSpeed() {
+        // Elf and Dark Elf speed stat
+        if(this.race == "Elf" || this.race == "Dark Elf") {
+            this.speed = 1
+        }
+        // Orc, Ogre and Undead speed stat
+        else if(this.race == "Orc" || this.race == "Ogre" || this.race == "Undead") {
+            this.speed = -3
+        }
+        // Halfling and Goblin speed stat
+        else if(this.race == "Halfling" || this.race == "Goblin") {
+            this.speed = 2
+        }
+        // Any other Race speedstat
+        else {
+            this.speed = 0
+        }
+    }
     // dodge chance function
     dodgeChance() {
         // Elf and Dark Elf dodgeChance()
         if(this.race == "Elf" || this.race == "Dark Elf") {
-            //  60% chance to dodge
-            if(Math.ceil(Math.random() * 100) > 40) {
+            //  50% chance to dodge
+            if(Math.ceil(Math.random() * 100) > 50) {
                 return true
             }
-            else
-            {
+            else {
                 return false
             }
         }
@@ -73,6 +90,24 @@ class NPC {
     // }
 };
 
+const goblin = new NPC(16, 4, 0, "Goblin")
+const ogre = new NPC(25, 6, 4, "Ogre")
+const darkElf = new NPC(19, 5, 1, "Dark Elf")
+const skeleton = new NPC(14, 4, 6, "Undead")
+skeleton.name = "Skeleton"
+const lich = new NPC(12, 4, 6, "Undead")
+lich.name = "Lich"
+const halfling = new NPC(18, 4, 0, "Halfling")
+const elf = new NPC(19, 5, 1, "Elf")
+const orc = new NPC(25, 6, 4, "Orc")
+const human = new NPC(20, 4, 2, "Human")
+
+const enemy = [goblin, ogre, darkElf, skeleton, lich, halfling, elf, orc, human]
+
+for (let i = 0; i < enemy.length; i++) {
+    enemy[i].changeSpeed()
+}
+
 window.onload = function () {
     modal.style.display = "block"
     for (let i = 0; i < victoryBonus.length; i++) {
@@ -89,5 +124,11 @@ for (let i = 0; i < difficultySet.length; i++) {
     difficultySet[i].onclick = function() {
         diff = difficultySet[i].value
         modal.style.display = "none"
+        // create the players character based on race chosen
+        // base stats 30hp 5att 4def 1speed
     }
 }
+
+// fight function
+// choose how many opponents based on the difficulty
+// randomly choose those opponents for the pool of 9 enemys
