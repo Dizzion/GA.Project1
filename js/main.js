752ef27f47df755ab4178e5a936628ffa41f78b7
AShaver43 <gofrodude_197@hotmail.com>
2020-07-13 23:13:17 -0700

    1st Modal Done

--- js/main.js
+++ js/main.js
@@ -1,88 +1,73 @@
 // modals and input are my challenges to get done
 const modal = document.querySelector("#myModal")
-const span = document.querySelector(".close")
 const raceInput = document.querySelector("#raceSelector")
 const raceSelect = document.querySelector("#raceSelect")
 let playerRace = "";
+const difficultySet = document.querySelectorAll(".diffSet")
+let diff = ""
 
 class NPC {
-    constructor(hp, atk, def, race)
-    {
-        this.health = hp;
-        this.attack = atk;
-        this.defense = def;
-        this.race = race;
-        this.name = race;
-        this.life = true;
-        this.speed = 0;
+    constructor(hp, atk, def, race) {
+        this.health = hp
+        this.attack = atk
+        this.defense = def
+        this.race = race
+        this.name = race
+        this.life = true
+        this.speed = 0
     }
     // dodge chance function
-    dodgeChance()
-    {
+    dodgeChance() {
         // Elf and Dark Elf dodgeChance()
-        if(this.race == "Elf" || this.race == "Dark Elf")
-        {
+        if(this.race == "Elf" || this.race == "Dark Elf") {
             //  60% chance to dodge
-            if(Math.ceil(Math.random() * 100) > 40)
-            {
-                return true;
+            if(Math.ceil(Math.random() * 100) > 40) {
+                return true
             }
             else
             {
-                return false;
+                return false
             }
         }
         // Orc, Ogre and Undead dodgeChance()
-        else if(this.race == "Orc" || this.race == "Ogre" || this.race == "Undead")
-        {
+        else if(this.race == "Orc" || this.race == "Ogre" || this.race == "Undead") {
             //  15% chance to dodge
-            if(Math.floor(Math.random() * 100) > 85)
-            {
-                return true;
+            if(Math.floor(Math.random() * 100) > 85) {
+                return true
             }
-            else
-            {
-                return false;
+            else {
+                return false
             }
         }
         // Halfling and Goblin dodgeChance()
-        else if(this.race == "Halfling" || this.race == "Goblin")
-        {
+        else if(this.race == "Halfling" || this.race == "Goblin") {
             //  65% Chance to dodge
-            if(Math.ceil(Math.random() * 100) > 35)
-            {
-                return true;
+            if(Math.ceil(Math.random() * 100) > 35) {
+                return true
             }
-            else
-            {
-                return false;
+            else {
+                return false
             }
         }
         // Any other Race dodgeChance()
-        else
-        {
+        else {
             //  35% Chance to dodge
-            if(Math.ceil(Math.random() * 100) > 65)
-            {
-                return true;
+            if(Math.ceil(Math.random() * 100) > 65) {
+                return true
             }
-            else
-            {
-                return false;
+            else {
+                return false
             }
         }
     }
-    // deathCheck()
-    // {
+    // deathCheck() {
     //     // check if this.life == true or false at the end of the fight function
     //     // if dead shift this combatant out of the enemy array
     // },
-    // dmgTaken(num)
-    // {
+    // dmgTaken(num) {
     //     // minus the damage taken from the objects HP
     // }
-    // dealDMG()
-    // {
+    // dealDMG() {
     //     // calculate the damage this combatant will do
     // }
 };
@@ -90,11 +75,15 @@ class NPC {
 window.onload = function () {
     modal.style.display = "block"
 }
-span.onclick = function() {
-    modal.style.display = "none"
-}
+
 raceInput.onclick = function() {
     playerRace = raceSelect.options[raceSelect.selectedIndex].innerText
     raceSelect.disabled = true
-    console.log(raceSelect.disabled)
+}
+
+for (let i = 0; i < difficultySet.length; i++) {
+    difficultySet[i].onclick = function() {
+        diff = difficultySet[i].value
+        modal.style.display = "none"
+    }
 }
